b0b73a91bf7fbd680f128aef266a60e665ae0ce2
Jason Weaver <119889739+Jweaver74@users.noreply.github.com>
2023-02-11 22:11:34 -0600

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 var randomTopic = topics[Math.floor(Math.random() *topics.lenght)];
 
@@ -17,28 +15,10 @@ if (randomTopic === 'HTML') {
 } else if (randomTopic === 'Git') {
   console.log("Let's study Git!");
 } else if (randomTopic === 'JavaScript') {
-=======
-var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
-
-for(var x = 0; x < topics.length; x++) {
-    console.log(topics[x]);
-   }
-   
-/*
-if (topic === 'HTML') {
-  console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
-  console.log("Let's study CSS!");
-} else if (topic === 'Git') {
-  console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
->>>>>>> 0ab59fc6511c5bbda4733d5f0eeeac2d0a9e1e73
-  console.log("Let's study JavaScript!");
+    console.log("Let's study JavaScript!");
 } else {
-  console.log('Please try again!');
-}
-<<<<<<< HEAD
-
+    console.log('Please try again!');
+   }
 }
 
 console.log('Here are the topics we learned through Prework');
@@ -46,8 +26,3 @@ listTopics( )
 console.log('Which topic should we study first?');
 selectTopic()
 
-
-
-=======
-*/
->>>>>>> 0ab59fc6511c5bbda4733d5f0eeeac2d0a9e1e73
